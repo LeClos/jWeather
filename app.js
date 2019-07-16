f9fd882fa83abab1e1a8c48f837774238f6bbb01
LeClos <jared.leclos@gmail.com>
2019-07-16 14:53:34 +0800

    theme changes

--- app.js
+++ app.js
@@ -32,6 +32,13 @@ window.addEventListener('load', () => {
           temperatureDegree.textContent = Math.round(((temperature - 32) * (5 / 9)));
           temperatureDescription.textContent = summary;
           locationTimezone.textContent = data.timezone;
+          // making location look nicer
+          var theLocation = data.timezone;
+          var timezone1 = theLocation.split('/');
+          var first = timezone1[1];
+          var second = timezone1[0];
+          var newWord = first.concat(", ", second);
+          locationTimezone.innerHTML = newWord;
           // Set icons
           setIcons(icon, document.querySelector('.icon'));
 
@@ -62,4 +69,49 @@ window.addEventListener('load', () => {
     skycons.play();
     return skycons.set(iconID, Skycons[currentIcon]);
   }
+});
+
+// getting time
+var d = new Date();
+document.getElementById("demo").innerHTML = d;
+
+// Function to change webpage background color
+function changeBodyBg(color) {
+  document.body.style.background = color;
+}
+
+// Function to change button background color
+function changeButtonBg(color) {
+  document.getElementById("theme").style.background = color;
+}
+
+// changing themes
+var but = document.querySelector('#theme');
+but.addEventListener("click", function (event) {
+  if (document.body.style.background != 'orange') {
+    changeBodyBg('orange')
+    changeButtonBg('linear-gradient(rgb(47, 150, 163), rgb(48, 62, 143))');
+    document.getElementById("theme").style.color = "white";
+    document.getElementById("degree").style.color = "black";
+    document.getElementById("symbol").style.color = "black";
+    document.getElementById("des").style.color = "black";
+    document.getElementById("tz").style.color = "black";
+    document.getElementById("info").style.color = "black";
+    document.getElementById("demo").style.color = "black";
+    document.getElementById("mybtn").style.background = "orange";
+    document.getElementById("mybtn").style.color = "black";
+  } else {
+    changeBodyBg('linear-gradient(rgb(47, 150, 163), rgb(48, 62, 143))');
+    changeButtonBg('orange');
+    document.getElementById("theme").style.color = "black";
+    document.getElementById("degree").style.color = "rgb(255, 204, 95)";
+    document.getElementById("symbol").style.color = "rgb(255, 204, 95)";
+    document.getElementById("des").style.color = "white";
+    document.getElementById("tz").style.color = "white";
+    document.getElementById("info").style.color = "white";
+    document.getElementById("demo").style.color = "white";
+    document.getElementById("mybtn").style.background = 'linear-gradient(rgb(47, 150, 163), rgb(48, 62, 143))';
+    document.getElementById("mybtn").style.color = "white";
+  }
+
 });
